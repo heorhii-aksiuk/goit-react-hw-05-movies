00e970040b6343cd9aec951561e7834d5dae1617
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-25 00:25:57 +0200

    Header mvp

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './Header.module.css';
+
 // import PropTypes from 'prop-types';
 
-function Header(props) {
+function Header() {
   return (
-    <header>
-      <nav></nav>
+    <header className={s.header}>
+      <nav className={s.navigation}>
+        <NavLink exact to="/" className={s.link} activeClassName={s.activeLink}>
+          Home
+        </NavLink>
+        <NavLink to="/movies" className={s.link} activeClassName={s.activeLink}>
+          Movies
+        </NavLink>
+      </nav>
     </header>
   );
 }
