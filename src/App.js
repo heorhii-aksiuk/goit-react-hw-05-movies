baea3fc4684ebb69bbc6b920b4c461ec5ee620e0
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-28 22:24:34 +0200

    lazy loading realization

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import HomePage from './views/HomePage';
-import MoviePage from './views/MoviePage';
-import MovieDetailsPage from './views/MovieDetailsPage';
 import MovieCard from './components/MovieCard/MovieCard';
-import NodFoundPage from './views/NodFoundPage';
+const HomePage = lazy(() => import('./views/HomePage'));
+const MoviePage = lazy(() => import('./views/MoviePage'));
+const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage'));
+const NodFoundPage = lazy(() => import('./views/NodFoundPage'));
 
 function App() {
   return (
     <>
       <Header />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
 
-        <Route exact path="/movies">
-          <MoviePage />
-        </Route>
+          <Route exact path="/movies">
+            <MoviePage />
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <MovieDetailsPage>
-            <MovieCard />
-          </MovieDetailsPage>
-        </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage>
+              <MovieCard />
+            </MovieDetailsPage>
+          </Route>
 
-        <Route>
-          <NodFoundPage />
-        </Route>
-      </Switch>
+          <Route>
+            <NodFoundPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </>
   );
 }
