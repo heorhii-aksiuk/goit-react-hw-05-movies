0a8f5e8efe8ec4f300b1a7f0542b72796de4ab88
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-28 22:32:52 +0200

    add chunk names

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, { lazy, Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import MovieCard from './components/MovieCard/MovieCard';
-const HomePage = lazy(() => import('./views/HomePage'));
-const MoviePage = lazy(() => import('./views/MoviePage'));
-const MovieDetailsPage = lazy(() => import('./views/MovieDetailsPage'));
-const NodFoundPage = lazy(() => import('./views/NodFoundPage'));
+const HomePage = lazy(() =>
+  import('./views/HomePage' /* webpackChunkName: "HomePage" */),
+);
+const MoviePage = lazy(() =>
+  import('./views/MoviePage' /* webpackChunkName: "MoviePage" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import('./views/MovieDetailsPage' /* webpackChunkName: "MovieDetailsPage" */),
+);
+const NodFoundPage = lazy(() =>
+  import('./views/NodFoundPage' /* webpackChunkName: "NodFoundPage" */),
+);
 
 function App() {
   return (
