3404485b5d752d965ed80f8a22b16746bc160a5b
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-01 20:38:34 +0200

    loader timeout time change

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -8,7 +8,7 @@ function Loader() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoaderTimeOut(true);
-    }, 1000);
+    }, 500);
     return () => clearTimeout(timer);
   }, []);
 
