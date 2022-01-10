7b8965e2fe99546812cc5861084aef42e4f3d228
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-10 23:36:55 +0200

    play with styles

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -5,7 +5,9 @@ import s from './Section.module.css';
 function Section({ title, children }) {
   return (
     <section className={s.section}>
-      <h2 className={s.title}>{title}</h2>
+      <div className={s.titleWrapper}>
+        <h2 className={s.title}>{title}</h2>
+      </div>
       {children}
     </section>
   );
